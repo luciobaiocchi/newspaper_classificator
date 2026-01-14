f9618c849cf4541d7bf512c06af134424e458821
leonardopassafiume <leonardo.passafiume@gmail.com>
2026-01-14 16:55:15 +0100

    testing

--- main.py
+++ main.py
@@ -40,6 +40,45 @@ print("Creazione colonna 'text'...")
 X['text'] = X['title'] + " " + X['title'] + " " + X['article']
 X_test['text'] = X_test['title'] + " " + X_test['title'] + " " + X_test['article']
 
+def process_dates(df, col_name='date'):
+    # 1. 'coerce': Se trovi "0000-00-00", trasformalo in NaT (Null) invece di crashare
+    df[col_name] = pd.to_datetime(df[col_name], errors='coerce')
+    
+    # 2. Estrai i componenti base
+    # Usiamo .dt direttamente. Dove la data è NaT, otterremo NaN (che va bene per XGBoost!)
+    df['hour'] = df[col_name].dt.hour
+    df['month'] = df[col_name].dt.month
+    df['day_of_week'] = df[col_name].dt.dayofweek
+    
+    # 3. Cyclical Encoding (Seno e Coseno)
+    # Se 'hour' è NaN, anche il seno/coseno sarà NaN.
+    # XGBoost gestisce i NaN automaticamente imparando come trattarli.
+    
+    # ORA
+    df['hour_sin'] = np.sin(2 * np.pi * df['hour'] / 24.0)
+    df['hour_cos'] = np.cos(2 * np.pi * df['hour'] / 24.0)
+    
+    # MESE
+    df['month_sin'] = np.sin(2 * np.pi * df['month'] / 12.0)
+    df['month_cos'] = np.cos(2 * np.pi * df['month'] / 12.0)
+    
+    # GIORNO
+    df['day_sin'] = np.sin(2 * np.pi * df['day_of_week'] / 7.0)
+    df['day_cos'] = np.cos(2 * np.pi * df['day_of_week'] / 7.0)
+    
+    # Opzionale: Riempiamo i NaN con 0 per pulizia (0 è il centro del cerchio, "neutro")
+    # Oppure lasciamoli NaN, XGBoost se la cava. Per sicurezza riempiamoli con 0.
+    new_cols = ['hour_sin', 'hour_cos', 'month_sin', 'month_cos', 'day_sin', 'day_cos']
+    df[new_cols] = df[new_cols].fillna(0)
+    
+    return df
+
+
+X = process_dates(X, col_name='timestamp') 
+X_test = process_dates(X_test, col_name='timestamp')
+
+time_features = ['hour_sin', 'hour_cos', 'month_sin', 'month_cos', 'day_sin', 'day_cos']
+
 # 5. PREPARAZIONE VOCABOLARIO (La tua logica custom)
 print("Estrazione vocabolario specifico...")
 TOP_K_PER_CLASS = 7000 
@@ -73,17 +112,33 @@ master_vectorizer = TfidfVectorizer(
 preprocessor_custom = ColumnTransformer(transformers=[
     ('source', encoder, ['source']),
     ('text', master_vectorizer, 'text'),
+    ('time', StandardScaler(), time_features),
+    ('page_rank', StandardScaler(), ['page_rank'])
 ], remainder='drop', n_jobs=1) # Messo a 1 per sicurezza in debug
 
 # 7. MODELLO (Logistic Regression o XGBoost)
 # Usiamo LogisticRegression come nel tuo esempio (o cambia con clf XGBoost)
+
 clf = LogisticRegression(
     random_state=RANDOM_SEED,
     C=1, penalty='l2', 
     solver='saga', 
     class_weight='balanced', 
-    n_jobs=-1
+    n_jobs=-1,
+    max_iter=1000
 )
+
+#clf = XGBClassifier(
+#    n_estimators=1000,     
+#    learning_rate=0.1,     
+#    max_depth=6,            
+#    subsample=0.8,          
+#    colsample_bytree=0.8,   
+#    n_jobs=-1,              
+#    random_state=42,
+#    tree_method="hist"      
+#    
+#)
 
 # 8. PIPELINE COMPLETA (Fit & Predict)
 full_pipeline = Pipeline([
